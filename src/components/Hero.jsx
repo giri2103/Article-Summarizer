0b829e8038eb57cd2bad5af3c3f2ab44ee247b5a
giri2103 <anishkumargiri.ak@gmail.com>
2023-12-27 15:14:54 +0530

    Added copy to clipboard functionality, Used the services from top to bottom, Made it fully functional

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,28 +1,31 @@
 import React from "react";
+
 import { logo } from "../assets";
 
 const Hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
       <nav className="flex justify-between items-center w-full mb-10 pt-3">
-        <img src={logo} alt="QuickSummarize" className="w-28 object-contain" />
+        <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
+
         <button
           type="button"
           onClick={() =>
-            window.open("https://github.com/giri2103/QuickSummarize")
+            window.open("https://github.com/giri2103/Article-Summarizer", "_blank")
           }
           className="black_btn"
         >
-          Github
+          GitHub
         </button>
       </nav>
+
       <h1 className="head_text">
         Summarize Articles with <br className="max-md:hidden" />
-        <span className="orange_gradient">OpenAI GPT-4</span>
+        <span className="orange_gradient ">OpenAI GPT-4</span>
       </h1>
       <h2 className="desc">
-        Simplify your reading with SumZ, An open-source article summarizer that
-        transforms lengthy articles into clear and concise summaries
+        Simplify your reading with Summize, an open-source article summarizer
+        that transforms lengthy articles into clear and concise summaries
       </h2>
     </header>
   );
--- src/services/article.js
+++ src/services/article.js
@@ -7,18 +7,21 @@ export const articleApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com/",
     prepareHeaders: (headers) => {
-      headers.set("X-RapidAPI-key", rapidApiKey);
+      headers.set("X-RapidAPI-Key", rapidApiKey);
       headers.set(
         "X-RapidAPI-Host",
         "article-extractor-and-summarizer.p.rapidapi.com"
       );
+
       return headers;
     },
   }),
   endpoints: (builder) => ({
     getSummary: builder.query({
+      // encodeURIComponent() function encodes special characters that may be present in the parameter values
+      // If we do not properly encode these characters, they can be misinterpreted by the server and cause errors or unexpected behavior. Thus that RTK bug
       query: (params) =>
-        "/summarize?url=${encodeURIComponent(params.articleUrl)}&length=3",
+        `summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`,
     }),
   }),
 });
